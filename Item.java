02342a37621e7930051440ded1eb1e80f5063c88
programmer746 <151440454+programmer746@users.noreply.github.com>
2024-10-08 19:27:38 +0530

    Add files via upload

---
+++ Item.java
@@ -0,0 +1,14 @@
+package com.example.library.model;
+
+public class Item {
+    private String title;
+
+    // Getter and setter for title
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
