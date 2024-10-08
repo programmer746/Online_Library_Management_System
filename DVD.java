0ca0a621fd74fba921594a4ed7b2db439789794d
programmer746 <151440454+programmer746@users.noreply.github.com>
2024-10-08 19:27:12 +0530

    Add files via upload

---
+++ DVD.java
@@ -0,0 +1,19 @@
+package com.example.library.model;
+
+public class DVD {
+	private String director;
+    private String regionCode;
+	public String getDirector() {
+		return director;
+	}
+	public void setDirector(String director) {
+		this.director = director;
+	}
+	public String getRegionCode() {
+		return regionCode;
+	}
+	public void setRegionCode(String regionCode) {
+		this.regionCode = regionCode;
+	}
+
+}
